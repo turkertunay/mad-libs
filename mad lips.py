888dd85b229fb37ba77aa36e2816cefe0b81d30d
turkertunay <75833839+turkertunay@users.noreply.github.com>
2023-02-23 18:03:51 +0300

    Add files via upload

---
+++ mad lips.py
@@ -0,0 +1,12 @@
+place = input("place?: ")
+color = input("color?: ")
+color_2 = input("another color?: ")
+sport =input("sport?: ")
+friend = input("friend?: ")
+feeling = input("feeling?: ")
+
+
+print("You are in", place)
+print("You are wearing a", color, "shirt and a", color_2, "pair of trousers")
+print("You are playing", sport, "with", friend)
+print("You are feeling", feeling)
